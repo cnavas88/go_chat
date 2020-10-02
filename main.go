788490a86e17a59a8c382c1826b095291fb4754b
Carlos Navas <dante.maniacs@gmail.com>
2020-10-02 10:50:58 +0200

    load template once

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template" // to load templates when init program
 	"log"
 	"net/http"
@@ -14,12 +15,16 @@ type templateHandler struct {
 	templ    *template.Template // variable to store templates
 }
 
+var port string = ":8080"
+var templatesFolder string = "templates"
+
 func main() {
 	t := templateHandler{filename: "chat.html"}
 
 	http.HandleFunc("/", t.ServeHTTP)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	// Start the web server
+	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
@@ -28,9 +33,11 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// If once is called multiple times, only the
 	// first call invoke func()
 	t.once.Do(func() {
+		fmt.Println(filepath.Join(templatesFolder, t.filename))
 		// Must method wraps a call to returning template, err parameters
 		// if there are an error, must execute panic function
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+		t.templ = template.Must(template.ParseFiles(filepath.Join(templatesFolder, t.filename)))
 	})
+
 	t.templ.Execute(w, nil)
 }
