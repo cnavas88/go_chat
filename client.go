38f9b5a6b9c7d4b465b149251d588cdafd63f425
Carlos Navas <dante.maniacs@gmail.com>
2020-10-05 19:33:07 +0200

    adding read and write message in socket channgel

--- client.go
+++ client.go
@@ -9,3 +9,24 @@ type client struct {
 	send   chan []byte     // is a channel on which messages are sent
 	room   *room           // is the room this client is chatting in
 }
+
+func (c *client) read() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.socket.Close()
+	for msg := range c.send {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
+}
